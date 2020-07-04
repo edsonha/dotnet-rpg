16a767816ab11c91df2687d597dcf43575f027cc
edsonha <edsonha@gmail.com>
2020-07-04 12:12:31 +0800

    Delete implementation

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -33,6 +33,24 @@ namespace dotnet_rpg.Services.CharacterService
       return serviceResponse;
     }
 
+    public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+    {
+      ServiceResponse<List<GetCharacterDto>> serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+      try
+      {
+        Character character = characters.First(person => person.Id == id);
+        characters.Remove(character);
+        serviceResponse.Data = (characters.Select(c => _mapper.Map<GetCharacterDto>(c))).ToList();
+      }
+      catch (Exception ex)
+      {
+        serviceResponse.Success = false;
+        serviceResponse.Message = ex.Message;
+      }
+      return serviceResponse;
+    }
+
+
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
       ServiceResponse<List<GetCharacterDto>> serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
