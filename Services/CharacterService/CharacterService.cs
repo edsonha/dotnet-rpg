e6dfbf7cfb63f9c5c0a52a754ed46ef72839057e
edsonha <edsonha@gmail.com>
2020-07-04 11:36:53 +0800

    Create automapper profile to create the mapping

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -24,22 +24,22 @@ namespace dotnet_rpg.Services.CharacterService
     public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
     {
       ServiceResponse<List<GetCharacterDto>> serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-      characters.Add(newCharacter);
-      serviceResponse.Data = characters;
+      characters.Add(_mapper.Map<Character>(newCharacter));
+      serviceResponse.Data = (characters.Select(c => _mapper.Map<GetCharacterDto>(c))).ToList();
       return serviceResponse;
     }
 
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
       ServiceResponse<List<GetCharacterDto>> serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-      serviceResponse.Data = characters;
+      serviceResponse.Data = (characters.Select(c => _mapper.Map<GetCharacterDto>(c))).ToList();
       return serviceResponse;
     }
 
     public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
     {
       ServiceResponse<GetCharacterDto> serviceResponse = new ServiceResponse<GetCharacterDto>();
-      serviceResponse.Data = characters.FirstOrDefault(person => person.Id == id);
+      serviceResponse.Data = _mapper.Map<GetCharacterDto>(characters.FirstOrDefault(person => person.Id == id));
       return serviceResponse;
     }
   }
